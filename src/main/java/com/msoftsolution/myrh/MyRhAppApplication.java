77aa0aca5ac735a77fd6631b88e687605c28c5ea
zakaria <mr.zakariadaoudi@gmail.com>
2022-05-08 12:57:11 +0100

    ù initial develo

---
+++ src/main/java/com/msoftsolution/myrh/MyRhAppApplication.java
@@ -0,0 +1,13 @@
+package com.msoftsolution.myrh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyRhAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyRhAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/msoftsolution/myrh/MyRhAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.msoftsolution.myrh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyRhAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
